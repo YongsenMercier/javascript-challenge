6a68162cd18ffd8b290e3a6450953104a7598fe3
Yongsen Mercier <yongsen.mercier@gmail.com>
2020-10-28 23:16:27 -0400

    added filter function

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,6 +1,6 @@
 // from data.js
 const tableData = data;
-//let tableMatch = null;
+let tableMatch = null;
 
 // set the reference to the table body and initialize filter flag
 let tbody = d3.select("tbody");
@@ -15,3 +15,41 @@ tableData.forEach(row => {
         cell.text(row[key]);
     }
 });
+
+let dateField = d3.select("#datetime");
+const button = d3.select("#filter-btn");
+
+
+// filter the table by date
+function filterDate(){
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // get the user's entered date value
+    let userDate = dateField.property("value");
+
+    // only filter if user entered a date
+    if(userDate){
+        // flag that table is filtered
+        filtered = 1;
+
+        // filter tableData for observations with matching dates
+        tableMatch = tableData.filter(obs => obs.datetime == userDate);
+
+        // wipe out the tbody to be able to write out new table
+        tbody.html("");
+
+        // fill in observations only where date matches user input
+        tableMatch.forEach(row => {
+            tbody.append("tr");
+        
+            for (key in row){
+                const cell = tbody.append("td");
+                cell.text(row[key]);
+            }
+        });
+    };
+}
+
+// define what happens when user clicks the buttons
+button.on("click", filterDate);
